d20b8b23b1d400c90cdfbfb1ac7dba88443a749a
TheEpicBlock <61842090+TheEpicBlock@users.noreply.github.com>
2020-08-01 21:54:20 +0200

    Tick mixin

--- src/main/java/net/fabricmc/example/mixin/ServerTickMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ServerTickMixin.java
@@ -1,15 +1,16 @@
 package net.fabricmc.example.mixin;
 
 import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		System.out.println("This line is printed by an example mod mixin!");
+@Mixin(MinecraftServer.class)
+public class ServerTickMixin {
+	@Inject(at = @At("HEAD"), method = "tickWorlds(Ljava/util/function/BooleanSupplier;)V")
+	private void tickInject(CallbackInfo info) {
+
 	}
 }
