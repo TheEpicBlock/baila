54d3639921319bcaf68354c96984482596aedbf9
comp500 <comp500@users.noreply.github.com>
2020-10-03 03:52:14 +0100

    Cache titles, clear when air, decrease range

--- src/main/java/nl/theepicblock/baila/BailaTickHandler.java
+++ src/main/java/nl/theepicblock/baila/BailaTickHandler.java
@@ -1,29 +1,33 @@
 package nl.theepicblock.baila;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.client.util.math.Vector3d;
 import net.minecraft.network.packet.s2c.play.TitleS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.World;
 
 public class BailaTickHandler {
     public static void tick(ServerPlayerEntity player) {
         ServerWorld world = player.getServerWorld();
 
-        HitResult blockHit = player.rayTrace(20,0,false);
+        HitResult blockHit = player.rayTrace(8,0,false);
         BlockState blockState = world.getBlockState(((BlockHitResult)blockHit).getBlockPos());
 
-        send(player, new TranslatableText(blockState.getBlock().getTranslationKey()));
+        if (!blockState.isAir()) {
+            send(player, new TranslatableText(blockState.getBlock().getTranslationKey()));
+        } else {
+            send(player, LiteralText.EMPTY);
+        }
     }
 
     public static void send(ServerPlayerEntity player, Text text) {
-        player.networkHandler.sendPacket(new TitleS2CPacket(TitleS2CPacket.Action.ACTIONBAR,text));
+        if (!text.equals(((PlayerCachedTitle) player).getCachedActionbarTitle())) {
+            player.networkHandler.sendPacket(new TitleS2CPacket(TitleS2CPacket.Action.ACTIONBAR,text));
+            ((PlayerCachedTitle) player).setCachedActionbarTitle(text);
+        }
     }
 }
